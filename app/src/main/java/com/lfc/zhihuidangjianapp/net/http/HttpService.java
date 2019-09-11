5608976cfa748c875f67a9a18c1fc6673eb204a7
郭剑 <gj@fengshang2002.com>
2019-09-11 15:05:06 +0800

    fix bug

--- app/src/main/java/com/lfc/zhihuidangjianapp/net/http/HttpService.java
+++ app/src/main/java/com/lfc/zhihuidangjianapp/net/http/HttpService.java
@@ -161,7 +161,7 @@ public interface HttpService {
      * @param token
      * @return
      */
-    @POST(ApiConstant.ROOT_URL + "weiXin2Pay/wxPayToApp")
+    @POST("weiXin2Pay/wxPayToApp")
     Observable<BaseResponse<WechatPay>> wxPayToApp(@QueryMap Map<String, Object> map,@Header("token") String token);
 
     /**
@@ -170,7 +170,7 @@ public interface HttpService {
      * @param token
      * @return
      */
-    @POST(ApiConstant.ROOT_URL + "alipay/alipayToApp")
+    @POST("alipay/alipayToApp")
     Observable<BaseResponse<AliPay>> alipayToApp(@QueryMap Map<String, Object> map,@Header("token") String token);
 
     /**
